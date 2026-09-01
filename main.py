695977a956eae2c33ef425dd3af47a6d8c41b377
Alexander Sambrook <alexsambrook@Alexanders-MacBook-Pro.local>
2026-09-01 13:17:35 +0100

    completed welcome screen

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 import streamlit as st
 
+from welcome_screen import welcome_screen
+
+welcome_screen()
--- welcome_screen.py
+++ welcome_screen.py
@@ -1,9 +1,44 @@
-st.title("The Business Conduct Guidlines Quiz")
+import streamlit as st
 
-user_input = st.text_input("Please enter your name in the box below to begin!")
+import re
 
-if user_input:
-    st.write(f"Hello, {user_input.title()}")
+class NameValidator:
+    def length_check(self, name: str) -> bool:
+        return 3 <= len(name.strip()) <= 30
+
+    def pattern_check(self, name: str) -> bool:
+        pattern = r"[A-Za-z\s'-]+"
+        return bool(re.fullmatch(pattern, name.strip()))
+
+    def error_handler(self, message: str):
+        st.error(message)
+
+    def validate_name(self, name: str) -> bool:
+        if not self.length_check(name):
+            self.error_handler("The name should be between 3 and 30 characters")
+            return False
+        elif not self.pattern_check(name):
+            self.error_handler("The name should only contain letters, hyphens, apostrophes and spaces")
+            return False
+        return True
+
+validator = NameValidator()
+
+def welcome_screen():
+    st.title("The Business Conduct Guidlines Quiz")
+   
+    st.write("""
+    Welcome! This Quiz is for employees of IBM to complete. It ensures all employees are aware of the organisation's values and guidelines.
+
+    You will be faced with a series of multiple choice questions testing your knowledge of the business conduct guidelines.
+    """)
+
+    name = st.text_input("Please enter your name in the box below to begin!")
+
+    if st.button("Begin Quiz!"):
+        if validator.validate_name(name):
+            st.session_state["name"] = name.title()
+            st.session_state["screen"] = "quiz_screen"
 
 
-ffef
+
