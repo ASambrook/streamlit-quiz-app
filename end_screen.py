331637bf73dc976f6bdc3c2383ccbad5a3a3e007
Alexander Sambrook <alexsambrook@Alexanders-MacBook-Pro.local>
2026-09-09 12:38:02 +0100

    docstrings

--- end_screen.py
+++ end_screen.py
@@ -2,6 +2,12 @@ import streamlit as st
 from storage import save_result
 
 def end_screen() -> None:
+    """
+    Displays the final screen of the quiz, congratulating the user and presenting them with their final score.
+    If the user's name and score have not yet been saved then they are written to the CSV file where the user will be greeted with a message confirming this.
+    There is also a `Download results CSV` button where the user can download the results CSV file, allowing them to view and store the history of results.
+    There is also a `Play again` button which allows the user to retry the quiz. This restores all the session state variables back to their default starting values, ensuring the user's new attempt is not affected by any previous attempts.
+    """
     name = st.session_state.name
     score = st.session_state.score
     total = st.session_state.get("total_questions", 10)
